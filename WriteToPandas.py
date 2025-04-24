117c885deceb569a24e8f0ad6f78a7d679a35552
Matthew Woodcock <68932282+matthewwoodc0@users.noreply.github.com>
2025-04-23 20:19:58 -0500

    Update WriteToPandas.py

--- WriteToPandas.py
+++ WriteToPandas.py
@@ -9,10 +9,6 @@ BAUD_RATE = 9600
 
 # Create a CSV file and ensure there are headers
 with open('temperature_log.csv', 'w', newline='') as file:
-    # Establish the writer
-    writer = csv.writer(file)
-    writer.writerow(['Time (ms)', 'Sensor Value', 'Volts', 'Degrees C'])
-
     # Connect to serial so you i can get the input stream
     ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1)
     time.sleep(2)  # Give Arduino time to reset and actually log
@@ -20,13 +16,17 @@ with open('temperature_log.csv', 'w', newline='') as file:
     print("Logging data... Press Ctrl+C to stop.")
     #Try catch so it doesnt break if something goes wrong and so we know when to stop it
     try:
+        # Establish the writer
+        writer = csv.writer(file)
+        writer.writerow(['Time (ms)', 'Sensor Value', 'Volts', 'Degrees C'])
+
         while True:
             line = ser.readline().decode('utf-8').strip()
 
             # Match format like: Time: 1299, Sensor Value: 123, Volts: 0.71, degrees c: 21.03
             match = re.match(r'Time: (\d+), Sensor Value: (\d+), Volts: ([\d.]+), degrees c: ([\d.]+)', line)
             if match:
-                row = [int(match[1]), int(match[2]), float(match[3]), float(match[4])]
+                row = [int(match.group(1)), int(match.group(2)), float(match.group(3)), float(match.group(4))]
                 print(row)
                 writer.writerow(row)
 
